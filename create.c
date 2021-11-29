e249f2d248a0dad572a14d3d6871368aadb5e3c9
Nicholas Imperius <nimperius200@gmail.com>
2021-11-28 21:37:58 -0500

    Create create.c

---
+++ create.c
@@ -0,0 +1,22 @@
+/*
+
+ESOF 3655 Project
+
+Nicholas Imperius
+Jimmy Tsang
+Jarrod Graseley
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+void createFile() {
+	
+	//Initialize file variable
+	FILE *
+	
+	
+	
+}
+	
