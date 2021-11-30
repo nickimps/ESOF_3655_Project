e519284b8a1f84b1829240f84004d3a95c39cb53
KimBokChoy <80291695+KimBokChoy@users.noreply.github.com>
2021-11-29 21:53:28 -0500

    Update removeText.c

--- removeText.c
+++ removeText.c
@@ -10,6 +10,25 @@ This file removes text from a .txt file
 
 */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
 void remove() {
+		//Initialize file variable and char arrays
+	char textName[800];
 	
+	//Get file name from the user
+	printf("Enter the textFile's name: ");
+	scanf("%s", textName);
+	
+	//Add comparison for .txt
+	
+	if (access(textName, F_OK) == 0) {
+		fopen(textName, "w");
+		printf("Text file has been cleared");
+	} 
+	else {
+		printf("Text file could not be found");
+	}
 }
