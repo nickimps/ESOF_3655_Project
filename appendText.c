a8fc32dd06fe6b265a8dabce3bb8312f15f139b0
Jarrod <jngrasle@lakeheadu.ca>
2021-11-30 05:05:16 -0500

    Update to append text file

--- appendText.c
+++ appendText.c
@@ -14,5 +14,24 @@ This file appends text to a .txt file
 #include <unistd.h>
 
 void append() {
+	//Get name of file to access
+	char source[800];
+	printf("Enter the file you would like to show: \n");
+	scanf("%s", source);
 	
+	//check if file exists
+	if (access(source, F_OK) != -1) {
+		//open the file in append mode
+		FILE* sourceFile = fopen(source, "a");
+		
+		//get data to append
+		char newData[800];
+		printf("Enter the text you would like to append: \n");
+		scanf("%s", newData);
+		
+		//send that data to the file
+		fputs(newData, sourceFile);
+		
+		fclose(sourceFile);
+	}
 }
