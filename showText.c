f1b488cb181ededd854187860c52f43004dac8fd
Jarrod <jngrasle@lakeheadu.ca>
2021-11-30 04:55:51 -0500

    Update to ShowText file

--- showText.c
+++ showText.c
@@ -14,5 +14,27 @@ This file shows the text of a .txt file
 #include <unistd.h>
 
 void show() {
+	//Get name of file to access
+	char source[800];
+	printf("Enter the file you would like to show: \n");
+	scanf("%s", source);
 	
+	//check if the file exists
+	if (access(source, F_OK) != -1) {
+		//open the file with read access
+		FILE* sourceFile = fopen(source, "r");
+		
+		if (sourceFile) {
+			//create temp variable
+			char temp;
+			
+			//read file until end of file (EOF)
+			while ((temp = fgetc(sourceFile)) != EOF) {
+				//send individual char to line until it reaches the end
+				putchar(temp);
+			}
+		}
+		//close the file
+		fclose(sourceFile);
+	}
 }
